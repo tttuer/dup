81497b840969aaf0e3cb4074b6dc266306ff9ad4
tttuer <tttuer1@naver.com>
2025-05-29 21:30:04 +0900

    안쓰는것 삭제

--- utils/whg.py
+++ utils/whg.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import json
@@ -13,7 +12,6 @@ from utils.settings import settings
 import time
 from domain.voucher import Voucher
 from datetime import datetime
-import dataclasses
 from domain.voucher import Company
 
 
