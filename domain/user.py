5d652abbe4a3d6803d8d714a0718e4fe59ed9b0b
tttuer <tttuer1@naver.com>
2025-06-16 10:25:47 +0900

    optional 설저

--- domain/user.py
+++ domain/user.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 from common.auth import Role
 
@@ -7,7 +8,7 @@ from common.auth import Role
 @dataclass
 class User:
     id: str
-    name: str
+    name: Optional[str] = None
     user_id: str
     password: str
     created_at: datetime
