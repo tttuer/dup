44d509e286fd602e7509c2e6e8e10f0df9fbfae4
tttuer <tttuer1@naver.com>
2025-06-16 15:14:52 +0900

    위치 수정

--- domain/user.py
+++ domain/user.py
@@ -8,9 +8,9 @@ from common.auth import Role
 @dataclass
 class User:
     id: str
-    name: Optional[str] = None
     user_id: str
     password: str
     created_at: datetime
     updated_at: datetime
     roles: list[Role]
+    name: Optional[str] = None
--- infra/db_models/user.py
+++ infra/db_models/user.py
@@ -9,12 +9,12 @@ from common.auth import Role
 
 class User(Document):
     id: str
-    name: Optional[str] = Field(default=None)
     user_id: str
     password: str
     created_at: datetime
     updated_at: datetime
     roles: list[Role]
+    name: Optional[str] = Field(default=None)
 
     class Settings:
         name = "users"
