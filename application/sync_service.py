d907c8f6b0c1c83077f96378c56785aaadf9a5c3
tttuer <tttuer1@naver.com>
2025-08-10 18:55:09 +0900

    redis ttl 설정

--- application/sync_service.py
+++ application/sync_service.py
@@ -13,7 +13,13 @@ class SyncService:
     async def set_sync_status(self, syncing: bool):
         now = datetime.now()
         status = SyncStatus(syncing=syncing, updated_at=now)
-        await self.redis.set(self.key, status.model_dump_json())
+        
+        if syncing:
+            # sync가 True일 때는 10분 TTL 설정 (자동 해제)
+            await self.redis.setex(self.key, 600, status.model_dump_json())
+        else:
+            # sync가 False일 때는 TTL 없이 저장
+            await self.redis.set(self.key, status.model_dump_json())
 
     async def get_current_status(self) -> bool:
         raw_data = await self.redis.get(self.key)
